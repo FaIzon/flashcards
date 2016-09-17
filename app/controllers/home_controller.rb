ad92dbb8fcc0d14cdf36776915ddd17d680aff9d
FaIzon <dimer90@gmail.com>
2016-09-17 20:33:04 +0700

    Simple train form - task 5

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,17 @@
 class HomeController < ApplicationController
   def index
+    @card = Card.review_date_expired.random_card
+    flash[:info] = 'Попробуй позже.' if @card.blank?
+  end
+
+  def check
+    @card = Card.find(params[:card][:id])
+    original_text = params[:card][:original_text]
+    if @card.update_review_date(original_text)
+      flash[:success] = 'Yes!'
+    else
+      flash[:danger] = 'No'
+    end
+    redirect_to root_url
   end
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -3,13 +3,33 @@ class Card  <  ActiveRecord::Base
 
   after_validation :check_translate_text
 
+  scope :review_date_expired, -> { where('review_date <= ?', Date.current) }
+  scope :random_card, -> { limit(1).order('RANDOM()').first }
+
   before_validation do
+    set_review_date
+  end
+
+  def update_review_date(random_card_original_text)
+    if words_are_equal?(random_card_original_text, original_text)
+      update_attribute(:review_date, set_review_date)
+      true
+    else
+      false
+    end
+  end
+
+  def set_review_date
     self.review_date = Time.now + 3.days
   end
 
-      def check_translate_text
-          if self.original_text.mb_chars.upcase.to_s == self.translated_text.mb_chars.upcase.to_s
-            errors.add(:original_text, 'Check the Word')
-          end
-     end
+  def words_are_equal?(first_word, second_word)
+      first_word.casecmp(second_word).zero?
   end
+
+  def check_translate_text
+    if self.original_text.mb_chars.upcase.to_s == self.translated_text.mb_chars.upcase.to_s
+      errors.add(:original_text, 'Check the Word')
+    end
+  end
+end
